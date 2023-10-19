b79e8926d614da88bcb87e4e279619fdf4002a2d
Eduardo Akira <e.akira@live.com>
2023-10-19 18:17:59 -0300

    feat: Adicionando o routes

---
+++ adapter/controller/routes/Routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"api-fundos-investimentos/adapter/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+func InitRoutes(
+	r *gin.RouterGroup,
+	userController controller.FundosControllerInterface) {
+
+}
