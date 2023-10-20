12527462d46e3ba849e41af223f77d681f1d594d
Eduardo Akira <e.akira@live.com>
2023-10-20 18:28:32 -0300

    feat: Se não for enviado um parametro é rota

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"api-fundos-investimentos/adapter/input/controller"
 	"api-fundos-investimentos/adapter/input/controller/routes"
+	"api-fundos-investimentos/adapter/input/listener"
 	"api-fundos-investimentos/adapter/output/externo"
 	"api-fundos-investimentos/adapter/output/queue"
 	"api-fundos-investimentos/adapter/output/repository"
@@ -11,6 +12,7 @@ import (
 	"api-fundos-investimentos/configuration/logger"
 	"context"
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -32,6 +34,11 @@ func main() {
 			"Error trying to connect to database, error=%s \n",
 			err.Error())
 		return
+	}
+
+	x := os.Args
+	if len(x) > 1 {
+		listener.Consume(x[1])
 	}
 
 	fundosController := initDependencies(database)
