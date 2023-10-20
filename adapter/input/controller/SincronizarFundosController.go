a4f08655a39a6a326121cff0708f83c41261b105
Eduardo Akira <e.akira@live.com>
2023-10-20 15:37:29 -0300

    feat: Adicionando no controller o Service para sincronizar os fundos

--- adapter/input/controller/SincronizarFundosController.go
+++ adapter/input/controller/SincronizarFundosController.go
@@ -1,14 +1,15 @@
 package controller
 
 import (
+	"api-fundos-investimentos/configuration/env"
 	"api-fundos-investimentos/configuration/logger"
 
 	"github.com/gin-gonic/gin"
 )
 
 func (fc *fundosControllerInterface) SincronizarFundos(c *gin.Context) {
-	logger.Info("Init SincronizarFundos", "sincrinizarFundos")
+	logger.Info("Init SincronizarFundos", "sincronizarFundos")
 
-	fc.service.GetFundosExternoService()
-	logger.Info("Finish SincronizarFundos", "sincrinizarFundos")
+	fc.service.QueueFundosExternoService(env.GetTopicSincronizar(), "update-all")
+	logger.Info("Finish SincronizarFundos", "sincronizarFundos")
 }
