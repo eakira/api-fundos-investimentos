063b4d1ba2fc3ba41d5dd6730ae745b75489d4f5
Eduardo Akira <e.akira@live.com>
2023-10-20 15:41:07 -0300

    feat; Adicionando o port para produce

--- application/port/output/FundosPort.go
+++ application/port/output/FundosPort.go
@@ -1,7 +1,13 @@
 package output
 
+import "api-fundos-investimentos/configuration/resterrors"
+
 type FundosPort interface {
 }
 
 type FundosExternoPort interface {
+}
+
+type FundosQueuePort interface {
+	Produce(string, string) *resterrors.RestErr
 }
