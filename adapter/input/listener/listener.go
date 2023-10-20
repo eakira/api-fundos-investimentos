30cd556d589b35da66aeabe436a18d54fea92101
Eduardo Akira <e.akira@live.com>
2023-10-20 18:29:25 -0300

    feat: Adicionando o consumer

---
+++ adapter/input/listener/listener.go
@@ -0,0 +1,45 @@
+package listener
+
+import (
+	"api-fundos-investimentos/configuration/env"
+	"api-fundos-investimentos/configuration/logger"
+	"api-fundos-investimentos/configuration/resterrors"
+	"fmt"
+	"log"
+
+	"github.com/IBM/sarama"
+)
+
+func initConsume() (sarama.Consumer, *resterrors.RestErr) {
+	config := sarama.NewConfig()
+	logger.Info(env.GetKafkaHost(), "kafka")
+
+	consumer, err := sarama.NewConsumer([]string{env.GetKafkaHost()}, config)
+	if err != nil {
+		logger.Error("NewConsumer err: ", err, "listener")
+		return nil, resterrors.NewNotFoundError("ConsumePartition err:")
+
+	}
+	return consumer, nil
+}
+
+func Consume(topic string) {
+	logger.Info(fmt.Sprintf("Init Listener: %s", topic), "listener")
+	consumer, _ := initConsume()
+	defer consumer.Close()
+
+	partitionConsumer, err := consumer.ConsumePartition(topic, 0, sarama.OffsetNewest)
+	if err != nil {
+		log.Fatal("ConsumePartition err: ", err)
+		resterrors.NewNotFoundError("ConsumePartition err:")
+	}
+	defer partitionConsumer.Close()
+
+	for message := range partitionConsumer.Messages() {
+		logger.Info(
+			fmt.Sprintf("[Consumer] partitionid: %d; offset:%d, value: %s\n",
+				message.Partition, message.Offset, string(message.Value)),
+			"sincronizarFundos")
+	}
+	logger.Info("Finish SincronizarFundos", "sincronizarFundos")
+}
