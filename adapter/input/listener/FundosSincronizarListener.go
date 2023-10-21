c022b9f64ec6596a2135937de8303210e850cae2
Eduardo Akira <e.akira@live.com>
2023-10-21 18:34:34 -0300

    feat: adicionando o json Unmarshal

--- adapter/input/listener/FundosSincronizarListener.go
+++ adapter/input/listener/FundosSincronizarListener.go
@@ -3,10 +3,21 @@ package listener
 import (
 	"api-fundos-investimentos/configuration/logger"
 	"api-fundos-investimentos/configuration/resterrors"
+	"encoding/json"
+	"fmt"
 )
 
-func FundosSincroniszarListener(message string) *resterrors.RestErr {
+func FundosSincroniszarListener(message []byte) *resterrors.RestErr {
 	logger.Info("Init FundosSincroniszarListener", "sincronizar")
+	var dat map[string]interface{}
+
+	err := json.Unmarshal(message, &dat)
+	if err != nil {
+		logger.Error("json Unmarshal error", err, "listener")
+	}
+	logger.Info(
+		fmt.Sprintf("json %v", dat),
+		"sincronizar")
 
 	logger.Info("Finish FundosSincroniszarListener", "sincronizar")
 
