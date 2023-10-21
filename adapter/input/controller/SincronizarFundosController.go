226f0ad3e44cd9f395dcca498f9e6ee78465ded3
Eduardo Akira <e.akira@live.com>
2023-10-21 07:54:48 -0300

    feat: Covertendo para dto os dados

--- adapter/input/controller/SincronizarFundosController.go
+++ adapter/input/controller/SincronizarFundosController.go
@@ -3,13 +3,19 @@ package controller
 import (
 	"api-fundos-investimentos/configuration/env"
 	"api-fundos-investimentos/configuration/logger"
+	"api-fundos-investimentos/core/dto"
 
 	"github.com/gin-gonic/gin"
 )
 
 func (fc *fundosControllerInterface) SincronizarFundos(c *gin.Context) {
 	logger.Info("Init SincronizarFundos", "sincronizarFundos")
+	dto := dto.FundosQueueDto{
+		Topic: env.GetTopicSincronizar(),
+		Queue: "update-all",
+		Data:  nil,
+	}
 
-	fc.service.QueueFundosSincronizarService(env.GetTopicSincronizar(), "update-all")
+	fc.service.QueueFundosSincronizarService(dto)
 	logger.Info("Finish SincronizarFundos", "sincronizarFundos")
 }
