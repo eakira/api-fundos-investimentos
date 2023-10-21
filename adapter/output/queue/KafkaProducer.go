24eb4d74a890e7a6fa829ce718dc0db08ebc8621
Eduardo Akira <e.akira@live.com>
2023-10-21 09:47:58 -0300

    feat: enviando o dto no lugar da string

--- adapter/output/queue/KafkaProducer.go
+++ adapter/output/queue/KafkaProducer.go
@@ -4,6 +4,7 @@ import (
 	"api-fundos-investimentos/configuration/env"
 	"api-fundos-investimentos/configuration/logger"
 	"api-fundos-investimentos/configuration/resterrors"
+	"api-fundos-investimentos/core/dto"
 	"fmt"
 
 	"github.com/IBM/sarama"
@@ -16,13 +17,13 @@ func NewProduce() *queueProduce {
 	return &queueProduce{}
 }
 
-func (nc *queueProduce) Produce(topic string, message string) *resterrors.RestErr {
+func (nc *queueProduce) Produce(dto dto.FundosQueueDto) *resterrors.RestErr {
 	logger.Info("Init Kafka Produce", "kafkaProduce")
 
 	producer, _ := initProduce()
 	defer producer.Close()
 
-	msg := &sarama.ProducerMessage{Topic: topic, Key: nil, Value: sarama.StringEncoder(message)}
+	msg := &sarama.ProducerMessage{Topic: dto.Topic, Key: nil, Value: sarama.StringEncoder(dto.Queue)}
 	partition, offset, err := producer.SendMessage(msg)
 	if err != nil {
 		logger.Error("SendMessage err:", err, "kafkaProduce")
@@ -30,7 +31,7 @@ func (nc *queueProduce) Produce(topic string, message string) *resterrors.RestEr
 	}
 
 	logger.Info(
-		fmt.Sprintf("Finish Kafka Produce partition id: %d; offset:%d, value: %s\n", partition, offset, message),
+		fmt.Sprintf("Finish Kafka Produce partition id: %d; offset:%d, value: %s\n", partition, offset, dto.Queue),
 		"kafkaProduce",
 	)
 	return nil
