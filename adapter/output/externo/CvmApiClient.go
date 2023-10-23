a6d696665b5b0cd1b10cfa338d087ae06d25f433
Eduardo Akira <e.akira@live.com>
2023-10-23 20:15:11 -0300

    feat: Iniciando o download dos arquivos

--- adapter/output/externo/CvmApiClient.go
+++ adapter/output/externo/CvmApiClient.go
@@ -4,8 +4,10 @@ import (
 	"api-fundos-investimentos/configuration/logger"
 	"api-fundos-investimentos/configuration/resterrors"
 	"os"
+	"strings"
 
 	"github.com/go-resty/resty/v2"
+	cp "github.com/otiai10/copy"
 )
 
 var (
@@ -16,13 +18,24 @@ var (
 type fundosClient struct{}
 
 func NewFundosClient() *fundosClient {
-
-	client = resty.New().SetBaseURL(os.Getenv(CVM_URL))
 	return &fundosClient{}
 }
 
-func (nc *fundosClient) DownloadArquivosCVMPort() *resterrors.RestErr {
+func (fc *fundosClient) DownloadArquivosCVMPort(folder string) *resterrors.RestErr {
 	logger.Info("Init DownloadArquivosCVMPort", "sincronizar")
+
+	folders := []string{
+		os.Getenv(CVM_URL),
+		folder,
+	}
+	folder = strings.Join(folders, "")
+	logger.Info(folder, "sincronizar")
+
+	err := cp.Copy("https://dados.cvm.gov.br/dados/FIP/DOC/INF_TRIMESTRAL/DADOS/", "storage/CVM")
+	if err != nil {
+		logger.Error("Erro ao copiar os arquivos", err, "sincronizar")
+		return resterrors.NewInternalServerError("Erro ao copiar os arquivo")
+	}
 
 	logger.Info("Finish DownloadArquivosCVMPort", "sincronizar")
 	return nil
