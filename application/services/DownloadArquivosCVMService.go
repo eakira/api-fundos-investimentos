0d738d94ef25d6af77f24872245a7d11db44a70e
Eduardo Akira <e.akira@live.com>
2023-10-23 07:19:28 -0300

    feat: Iniciando o service

---
+++ application/services/DownloadArquivosCVMService.go
@@ -0,0 +1,9 @@
+package services
+
+import "api-fundos-investimentos/configuration/logger"
+
+func (fs *fundosDomainService) DownloadArquivosCVMService() {
+	logger.Info("Init GetFundosExternoService", "sincronizarFundos")
+
+	logger.Info("Finish GetFundosExternoService", "sincronizarFundos")
+}
