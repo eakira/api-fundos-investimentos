f0620c433abefb78cfb301a2fdcc0e6f55d3d66c
Eduardo Akira <e.akira@live.com>
2023-10-23 07:17:16 -0300

    feat: Adicionando o controller

--- adapter/input/controller/DownloadArquivosCvmController.go
+++ adapter/input/controller/DownloadArquivosCvmController.go
@@ -1,19 +1,11 @@
 package controller
 
 import (
-	"api-fundos-investimentos/configuration/env"
 	"api-fundos-investimentos/configuration/logger"
-	"api-fundos-investimentos/core/dto"
 )
 
-func (fc *fundosControllerInterface) DownloadArquivosCvm() {
-	logger.Info("Init DownloadArquivosCvm", "sincronizarFundos")
-	dto := dto.FundosQueueDto{
-		Topic: env.GetTopicSincronizar(),
-		Queue: "update-all",
-		Data:  nil,
-	}
-
-	fc.service.QueueFundosSincronizarService(dto)
-	logger.Info("Finish SincronizarFundos", "sincronizarFundos")
+func (fc *fundosControllerInterface) DownloadArquivosCVMController() {
+	logger.Info("Init DownloadArquivosCVMController", "sincronizarFundos")
+	fc.service.DownloadArquivosCVMService()
+	logger.Info("Finish DownloadArquivosCVMController", "sincronizarFundos")
 }
