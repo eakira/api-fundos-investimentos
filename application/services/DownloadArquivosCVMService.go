5ba02fef157fb49d94bb52f258cf842bfc96bc6d
Eduardo Akira <e.akira@live.com>
2023-10-23 18:41:55 -0300

    feat: Enviando o nome da pasta

--- application/services/DownloadArquivosCVMService.go
+++ application/services/DownloadArquivosCVMService.go
@@ -2,8 +2,8 @@ package services
 
 import "api-fundos-investimentos/configuration/logger"
 
-func (fs *fundosDomainService) DownloadArquivosCVMService() {
+func (fs *fundosDomainService) DownloadArquivosCVMService(folder string) {
 	logger.Info("Init GetFundosExternoService", "sincronizarFundos")
-	fs.externo.DownloadArquivosCVMPort()
+	fs.externo.DownloadArquivosCVMPort(folder)
 	logger.Info("Finish GetFundosExternoService", "sincronizarFundos")
 }
