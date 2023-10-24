ef30bb4294b528b9a8a672ec43dfd3edd71a8874
Eduardo Akira <e.akira@live.com>
2023-10-24 13:44:05 -0300

    feat: Criando o sufixo de ano mês

--- application/services/QueueFundosSincronizarService.go
+++ application/services/QueueFundosSincronizarService.go
@@ -2,12 +2,61 @@ package services
 
 import (
 	"api-fundos-investimentos/adapter/output/model/response"
+	"api-fundos-investimentos/configuration/env"
 	"api-fundos-investimentos/configuration/logger"
+	"fmt"
+	"time"
 )
 
-func (fs *fundosDomainService) QueueFundosSincronizarService(response response.FundosQueueResponse) {
+func (fs *fundosDomainService) QueueFundosSincronizarService(tipo string) {
 	logger.Info("Init QueueFundosExternoService", "sincronizarFundos")
+	tipos := []string{
+		"cadastros",
+		"FIDC",
+		"FIE",
+		"FII",
+		"FIP",
+	}
+	datas := getArquivosSufixoAnoMes("2012-02-01", "2023-09-01")
 
-	fs.queue.Produce(response)
+	logger.Info(
+		fmt.Sprintf("json %v", datas),
+		"sincronizar")
+	for _, value := range tipos {
+		response := response.FundosQueueResponse{
+			Topic: env.GetTopicSincronizar(),
+			Queue: "update-all",
+			Data:  []string{value},
+		}
+		fs.queue.Produce(response)
+
+	}
+
 	logger.Info("Finish QueueFundosExternoService", "sincronizarFundos")
+}
+
+func getArquivosCadastros() []string {
+	return []string{
+		"FI/CAD/DADOS/cad_fi.csv",
+		"FI/CAD/DADOS/cad_fi_hist.zip",
+	}
+}
+func getArquivosBalancete() []string {
+	return []string{
+		"FI/CAD/DADOS/cad_fi.csv",
+		"FI/CAD/DADOS/cad_fi_hist.zip",
+	}
+}
+
+func getArquivosSufixoAnoMes(dataInicio string, dataFinal string) []string {
+	datas := []string{}
+	dataInicioConvertida, _ := time.Parse("2006-01-02", dataInicio)
+	dataFinalConvertida, _ := time.Parse("2006-01-02", dataFinal)
+	for dataInicioConvertida.Before(dataFinalConvertida) {
+		dataInicioConvertida = dataInicioConvertida.AddDate(0, 1, 0)
+		datas = append(datas, dataInicioConvertida.Format("200601"))
+	}
+
+	return datas
+
 }
