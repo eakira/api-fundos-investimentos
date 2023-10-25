bfd63b83fa7f1bc23faccb39da5010b57dd3680a
Eduardo Akira <e.akira@live.com>
2023-10-25 08:25:41 -0300

    feat: Adicionando o response

--- application/services/QueueFundosSincronizarService.go
+++ application/services/QueueFundosSincronizarService.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"api-fundos-investimentos/adapter/output/model/response"
 	"api-fundos-investimentos/configuration/env"
 	"api-fundos-investimentos/configuration/logger"
 	"fmt"
@@ -10,12 +11,12 @@ import (
 
 func (fs *fundosDomainService) QueueFundosSincronizarService(tipo string) {
 	logger.Info("Init QueueFundosExternoService", "sincronizarFundos")
-	files := []string{}
+	files := []response.FundosDownloadCvmFilesQueueResponse{}
 	logger.Info(time.Now().AddDate(0, -1, 0).Format("2006-01-02"), "sincronizarFundos")
 
 	switch tipo {
 	case "cadastros":
-		files = env.GetConfigCvmArquivosCadastros()
+		files = getArquivosCadastro(env.GetConfigCvmArquivosCadastros())
 
 	case "balancete":
 		files = getFilesName(env.GetConfigCvmBalancete())
@@ -65,18 +66,22 @@ func (fs *fundosDomainService) QueueFundosSincronizarService(tipo string) {
 	logger.Info("Finish QueueFundosExternoService", "sincronizarFundos")
 }
 
-func getFilesName(arquivos env.ArquivosCVM) []string {
+func getFilesName(arquivos env.ArquivosCVM) []response.FundosDownloadCvmFilesQueueResponse {
 	logger.Info("Init getFilesName", "sincronizarFundos")
 	sufixos := getArquivosSufixo(arquivos)
-
-	files := []string{}
+	files := []response.FundosDownloadCvmFilesQueueResponse{}
 	for _, value := range sufixos {
 		array := []string{
 			arquivos.Folder,
 			value,
 			arquivos.Extension,
 		}
-		files = append(files, strings.Join(array, ""))
+		file := response.FundosDownloadCvmFilesQueueResponse{
+			FileName:   strings.Join(array, ""),
+			Referencia: value,
+			Tipo:       arquivos.Tipo,
+		}
+		files = append(files, file)
 	}
 
 	logger.Info("Finish getFilesName", "sincronizarFundos")
@@ -96,4 +101,21 @@ func getArquivosSufixo(arquivo env.ArquivosCVM) []string {
 	}
 	return append(sufixos, dataInicioConvertida.Format(arquivo.Formato))
 
+}
+
+func getArquivosCadastro(env []string) []response.FundosDownloadCvmFilesQueueResponse {
+	files := []response.FundosDownloadCvmFilesQueueResponse{}
+
+	file := response.FundosDownloadCvmFilesQueueResponse{
+		FileName:   env[0],
+		Referencia: "2023",
+		Tipo:       env[2],
+	}
+	files = append(files, file)
+	file = response.FundosDownloadCvmFilesQueueResponse{
+		FileName:   env[0],
+		Referencia: "2022",
+		Tipo:       env[2],
+	}
+	return append(files, file)
 }
