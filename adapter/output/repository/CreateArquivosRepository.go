687419cdb8b5fda601ef4509b48e2f58060c2c24
Eduardo Akira <e.akira@live.com>
2023-10-25 13:53:04 -0300

    feat: salvando os arquivos no mongo

---
+++ adapter/output/repository/CreateArquivosRepository.go
@@ -0,0 +1,44 @@
+package repository
+
+import (
+	"api-fundos-investimentos/adapter/output/model/entity"
+	"api-fundos-investimentos/application/domain"
+	"api-fundos-investimentos/configuration/env"
+	"api-fundos-investimentos/configuration/logger"
+	"api-fundos-investimentos/configuration/resterrors"
+	"context"
+	"fmt"
+
+	"github.com/jinzhu/copier"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func (ur *fundosRepository) CreateArquivosRepository(
+	arquivosDomain domain.ArquivosDomain,
+) (*domain.ArquivosDomain, *resterrors.RestErr) {
+	logger.Info("Init createUser repository", "createArquivos")
+
+	collection := ur.databaseConnection.Collection(env.GetCollectionArquivos())
+
+	arquivosEntity := &entity.ArquivosEntity{}
+	copier.Copy(arquivosEntity, arquivosDomain)
+
+	result, err := collection.InsertOne(context.Background(), arquivosEntity)
+	logger.Info(
+		fmt.Sprintf("Domain: %v", arquivosDomain),
+		"createArquivos")
+
+	if err != nil {
+		logger.Error("Error trying to create user", err, "createArquivos")
+		return nil, resterrors.NewInternalServerError(err.Error())
+	}
+
+	arquivosEntity.ID = result.InsertedID.(primitive.ObjectID)
+
+	logger.Info("CreateUser repository executed successfully", "createArquivos")
+
+	arquivosDomainReturn := &domain.ArquivosDomain{}
+	copier.Copy(arquivosDomainReturn, arquivosEntity)
+
+	return arquivosDomainReturn, nil
+}
