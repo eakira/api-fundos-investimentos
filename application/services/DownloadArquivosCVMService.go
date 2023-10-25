2fdc6ad7e6e5748e1e8633cebbf2a3cc32c740c5
Eduardo Akira <e.akira@live.com>
2023-10-25 19:24:13 -0300

    feat: Iniciando o novo job

--- application/services/DownloadArquivosCVMService.go
+++ application/services/DownloadArquivosCVMService.go
@@ -1,10 +1,16 @@
 package services
 
 import (
+	"api-fundos-investimentos/adapter/output/model/request"
+	"api-fundos-investimentos/adapter/output/model/response"
 	"api-fundos-investimentos/application/constants"
 	"api-fundos-investimentos/application/domain"
+	"api-fundos-investimentos/configuration/env"
 	"api-fundos-investimentos/configuration/logger"
+	"encoding/json"
 	"time"
+
+	"github.com/jinzhu/copier"
 )
 
 func (fs *fundosDomainService) DownloadArquivosCVMService(arquivosDomain domain.ArquivosDomain) {
@@ -15,10 +21,30 @@ func (fs *fundosDomainService) DownloadArquivosCVMService(arquivosDomain domain.
 		return
 	}
 
+	salvando(fs, arquivosDomain)
+
+	proximoQueue(fs, arquivosDomain)
+
+	logger.Info("Finish GetFundosExternoService", "sincronizarFundos")
+}
+
+func salvando(fs *fundosDomainService, arquivosDomain domain.ArquivosDomain) {
 	arquivosDomain.UpdateAt = time.Now()
 	arquivosDomain.Download = true
 	arquivosDomain.Status = constants.DOWNLOAD
 	fs.repository.UpdateArquivosRepository(arquivosDomain)
+}
 
-	logger.Info("Finish GetFundosExternoService", "sincronizarFundos")
+func proximoQueue(fs *fundosDomainService, arquivosDomain domain.ArquivosDomain) {
+	arquivosRequest := request.FundosDownloadCvmFilesQueueRequest{}
+	copier.Copy(arquivosRequest, arquivosDomain)
+
+	data, _ := json.Marshal(arquivosRequest)
+	response := response.FundosQueueResponse{
+		Topic: env.GetTopicSincronizar(),
+		Queue: "update-all",
+		Data:  data,
+	}
+	fs.queue.Produce(response)
+
 }
