39f7e77341c71c72e908054ae6237c382e94889f
Eduardo Akira <e.akira@live.com>
2023-10-25 15:07:28 -0300

    feat: Fazendo o download dos arquivos da cvm

--- adapter/output/externo/CvmApiClient.go
+++ adapter/output/externo/CvmApiClient.go
@@ -1,13 +1,16 @@
 package externo
 
 import (
+	"api-fundos-investimentos/configuration/env"
 	"api-fundos-investimentos/configuration/logger"
 	"api-fundos-investimentos/configuration/resterrors"
+	"io"
+	"net/http"
 	"os"
+	"path"
 	"strings"
 
 	"github.com/go-resty/resty/v2"
-	cp "github.com/otiai10/copy"
 )
 
 var (
@@ -29,14 +32,45 @@ func (fc *fundosClient) DownloadArquivosCVMPort(file string) *resterrors.RestErr
 		file,
 	}
 	file = strings.Join(files, "")
-	logger.Info(file, "sincronizar")
 
-	err := cp.Copy("https://dados.cvm.gov.br/dados/FIP/DOC/INF_TRIMESTRAL/DADOS/", "storage/CVM")
+	//Buscando o arquivo
+	resp, err := http.Get(file)
 	if err != nil {
-		logger.Error("Erro ao copiar os arquivos", err, "sincronizar")
-		return resterrors.NewInternalServerError("Erro ao copiar os arquivo")
+		logger.Error("Erro ao tentar baixar o arquivo", err, "sincronizar")
+		return resterrors.NewInternalServerError("Erro ao tentar baixar o arquivo")
+	}
+	defer resp.Body.Close()
+
+	// Criando a pasta do arquivo
+	pasta := env.GetPathArquivosCvm()
+	err = os.MkdirAll(pasta, os.ModePerm)
+	if err != nil {
+		logger.Error("Erro ao tentar criar a pasta do arquivo", err, "sincronizar")
+		return resterrors.NewInternalServerError("Erro ao tentar criar a pasta do arquivo")
 	}
 
+	filepath := []string{
+		pasta,
+		path.Base(resp.Request.URL.String()),
+	}
+	file = strings.Join(filepath, "")
+
+	// Criando a pasta do arquivo
+	out, err := os.Create(file)
+	if err != nil {
+		logger.Error("Erro ao criar o arquivo", err, "sincronizar")
+		return resterrors.NewInternalServerError("Erro ao criar o arquivo")
+	}
+	defer out.Close()
+
+	// Copiando o arquivo
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		logger.Error("Erro ao salvar o arquivo", err, "sincronizar")
+		return resterrors.NewInternalServerError("Erro ao salvar o arquivo")
+	}
 	logger.Info("Finish DownloadArquivosCVMPort", "sincronizar")
+
 	return nil
+
 }
