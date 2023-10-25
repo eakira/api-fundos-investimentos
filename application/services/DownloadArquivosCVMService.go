ce329d7f9722b3ecb6b60359c2bf59270f8f4dee
Eduardo Akira <e.akira@live.com>
2023-10-25 18:52:18 -0300

    feat: Atualizando o arquivo

--- application/services/DownloadArquivosCVMService.go
+++ application/services/DownloadArquivosCVMService.go
@@ -1,13 +1,20 @@
 package services
 
 import (
+	"api-fundos-investimentos/application/constants"
 	"api-fundos-investimentos/application/domain"
 	"api-fundos-investimentos/configuration/logger"
+	"time"
 )
 
 func (fs *fundosDomainService) DownloadArquivosCVMService(arquivosDomain domain.ArquivosDomain) {
 	logger.Info("Init GetFundosExternoService", "sincronizarFundos")
-
 	fs.externo.DownloadArquivosCVMPort(arquivosDomain.Endereco)
+
+	arquivosDomain.UpdateAt = time.Now()
+	arquivosDomain.Download = true
+	arquivosDomain.Status = constants.DOWNLOAD
+	fs.repository.UpdateArquivosRepository(arquivosDomain)
+
 	logger.Info("Finish GetFundosExternoService", "sincronizarFundos")
 }
