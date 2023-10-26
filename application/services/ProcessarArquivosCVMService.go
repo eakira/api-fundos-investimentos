2a66ef912bf5985a1f5c181d3952feabd67349fc
Eduardo Akira <e.akira@live.com>
2023-10-26 19:14:03 -0300

    feat: Continuando com o processar arquivo

--- application/services/ProcessarArquivosCVMService.go
+++ application/services/ProcessarArquivosCVMService.go
@@ -1,77 +1,110 @@
 package services
 
 import (
-	"api-fundos-investimentos/adapter/output/model/request"
 	"api-fundos-investimentos/adapter/output/model/response"
 	"api-fundos-investimentos/application/constants"
 	"api-fundos-investimentos/application/domain"
 	"api-fundos-investimentos/configuration/env"
 	"api-fundos-investimentos/configuration/logger"
-	"bufio"
 	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"sync"
 	"time"
 
-	"github.com/jinzhu/copier"
+	"golang.org/x/text/encoding/charmap"
 )
 
 func (fs *fundosDomainService) ProcessarArquivosCVMService(arquivosDomain domain.ArquivosDomain) {
 	logger.Info("Init GetFundosExternoService", "sincronizarFundos")
 
-	switch arquivosDomain.TipoArquivo {
-	case "cadastros":
-		processaArquivoCadastro(arquivosDomain)
-	case "balancete":
-
-	case "cda":
-
-	case "informacoes-complementares":
-
-	case "extrato":
-
-	case "informacao-diaria":
-
-	case "lamina":
-
-	case "perfil-mensal":
-
-	}
+	processaArquivo(fs, arquivosDomain)
 
 	salvandoProcessar(fs, arquivosDomain)
-
-	//proximoQueue(fs, arquivosDomain)
-
 	logger.Info("Finish GetFundosExternoService", "sincronizarFundos")
 }
-func processaArquivoCadastro(arquivosDomain domain.ArquivosDomain) {
+
+func processaArquivo(fs *fundosDomainService, arquivosDomain domain.ArquivosDomain) {
+
+	cabecalhoChan := make(chan []string, 1)
+	linhaChan := make(chan []string, 1000)
+	jsonChan := make(chan []byte, 1000)
+	mensagemChan := make(chan response.FundosQueueResponse, 1000)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go processaCsv(linhaChan, cabecalhoChan, arquivosDomain)
+	go processarLinha(linhaChan, cabecalhoChan, arquivosDomain, jsonChan)
+	go proximoQueue(jsonChan, mensagemChan)
+	go fs.queue.ProduceLote(mensagemChan, &wg)
+
+	wg.Wait()
+
+}
+
+func processarLinha(
+	linhaChan chan []string,
+	cabecalhoChan chan []string,
+	arquivosDomain domain.ArquivosDomain,
+	jsonChan chan []byte,
+) {
+	cabecalho := <-cabecalhoChan
+	mapa := make(map[string]any)
+	mapa["collection"] = arquivosDomain.TipoArquivo
+	mapa["tipo-acao"] = "store"
+
+	for linha := range linhaChan {
+		for key, coluna := range cabecalho {
+			mapa[coluna] = linha[key]
+		}
+
+		json, err := json.Marshal(mapa)
+		if err != nil {
+			fmt.Println("tre")
+		}
+		jsonChan <- json
+	}
+	close(jsonChan)
+
+}
+
+func processaCsv(
+	linhaChan chan []string,
+	cabecalhoChan chan []string,
+	arquivosDomain domain.ArquivosDomain,
+) {
 	file, err := os.Open(env.GetPathArquivosCvm() + arquivosDomain.Endereco)
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 
-	reader := csv.NewReader(bufio.NewReader(file))
+	reader := csv.NewReader(charmap.ISO8859_15.NewDecoder().Reader(file))
 	reader.Comma = ';'
 	reader.FieldsPerRecord = -1
 	reader.LazyQuotes = true
+
+	cabecalhoChanArq, err := reader.Read()
+	cabecalhoChan <- cabecalhoChanArq
 	i := 0
 	for {
 		record, err := reader.Read()
 		if err == io.EOF {
 			break
-		}
-		if err != nil {
+		} else if err != nil {
+			logger.Error("error try reader.Read(): ", err, "processarArquivos")
 			log.Fatal(err)
 		}
-
-		fmt.Println(record)
+		linhaChan <- record
 		i++
+		fmt.Println(i)
 	}
-	fmt.Println(i)
+	close(linhaChan)
+	close(cabecalhoChan)
+	fmt.Println("Done")
 }
 
 func salvandoProcessar(fs *fundosDomainService, arquivosDomain domain.ArquivosDomain) {
@@ -81,16 +114,18 @@ func salvandoProcessar(fs *fundosDomainService, arquivosDomain domain.ArquivosDo
 	fs.repository.UpdateArquivosRepository(arquivosDomain)
 }
 
-func proximoQueue(fs *fundosDomainService, arquivosDomain domain.ArquivosDomain) {
-	arquivosRequest := request.FundosCvmArquivosQueueRequest{}
-	copier.Copy(arquivosRequest, arquivosDomain)
+func proximoQueue(
+	jsonChan chan []byte,
+	mensagemChan chan response.FundosQueueResponse,
+) {
 
-	data, _ := json.Marshal(arquivosRequest)
-	response := response.FundosQueueResponse{
-		Topic: env.GetTopicSincronizar(),
-		Queue: "update-all",
-		Data:  data,
+	for data := range jsonChan {
+		response := response.FundosQueueResponse{
+			Topic: env.GetTopicPersistenciaDados(),
+			Queue: "update-all",
+			Data:  data,
+		}
+		mensagemChan <- response
 	}
-	fs.queue.Produce(response)
 
 }
