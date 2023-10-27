930bf115ad05d8619803dffb11658c2aeac399b3
Eduardo Akira <e.akira@live.com>
2023-10-26 21:00:15 -0300

    feat: Adicionando às colections

--- configuration/env/Collections.go
+++ configuration/env/Collections.go
@@ -3,3 +3,35 @@ package env
 func GetCollectionArquivos() string {
 	return "arquivos"
 }
+
+func GetCollectionFundos() string {
+	return "fundo"
+}
+
+func GetCollectionBalancete() string {
+	return "balancete"
+}
+
+func GetCollectionCda() string {
+	return "cda"
+}
+
+func GetCollectionInformacoesComplementares() string {
+	return "informacoes-complementares"
+}
+
+func GetCollectionExtrato() string {
+	return "extrato"
+}
+
+func GetCollectionInformacaoDiaria() string {
+	return "informacao-diaria"
+}
+
+func GetCollectionLamina() string {
+	return "informacao-diaria"
+}
+
+func GetCollectionPerfilMensal() string {
+	return "perfil-mensal"
+}
