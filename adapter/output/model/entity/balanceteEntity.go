41c8cc0eb593e1e05a4192bd877c92206d01a6ac
Eduardo Akira <e.akira@live.com>
2023-10-27 10:27:38 -0300

    feat: Adicionando o id

--- adapter/output/model/entity/balanceteEntity.go
+++ adapter/output/model/entity/balanceteEntity.go
@@ -1,9 +1,12 @@
 package entity
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type BalacenteEntity struct {
-	CodigoConta     string `bson:"codigo_conta"`
-	FundoCnpj       string `bson:"fundo_cnpj"`
-	DataCompetencia string `bson:"data_competencia"`
-	PlanoContabil   string `bson:"plano_contabil"`
-	SaldoConta      string `bson:"saldo_conta"`
+	ID              primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	CodigoConta     string             `bson:"codigo_conta"`
+	FundoCnpj       string             `bson:"fundo_cnpj"`
+	DataCompetencia string             `bson:"data_competencia"`
+	PlanoContabil   string             `bson:"plano_contabil"`
+	SaldoConta      string             `bson:"saldo_conta"`
 }
