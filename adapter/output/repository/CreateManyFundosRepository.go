a35f3e4a3c5a8c6c67cd1cbb7ed04ac1523a5f2f
Eduardo Akira <e.akira@live.com>
2023-11-03 18:20:53 -0300

    feat: Mandando uma array para a repo

--- adapter/output/repository/CreateManyFundosRepository.go
+++ adapter/output/repository/CreateManyFundosRepository.go
@@ -6,6 +6,7 @@ import (
 	"api-fundos-investimentos/configuration/logger"
 	"api-fundos-investimentos/configuration/resterrors"
 	"context"
+	"fmt"
 
 	"github.com/jinzhu/copier"
 )
@@ -14,24 +15,30 @@ func (ur *fundosRepository) CreateManyFundosRepository(
 	fundosDomain []domain.FundosDomain,
 ) *resterrors.RestErr {
 
-	logger.Info("Init CreateFundosRepository", "createFundos")
+	logger.Info("Init CreateManyFundosRepository", "createFundos")
 
 	collection := ur.databaseConnection.Collection(env.GetCollectionFundos())
 
 	entity := make([]interface{}, len(fundosDomain))
 
 	copier.Copy(&entity, &fundosDomain)
+	fmt.Println(fundosDomain)
+	logger.Info("Init 2 CreateManyFundosRepository", "createFundos")
 
 	_, err := collection.InsertMany(context.Background(), entity)
+	logger.Info("Init 3 CreateManyFundosRepository", "createFundos")
 
 	if err != nil {
+		logger.Error("Error CreateManyFundosRepository", err, "createFundos")
 		return resterrors.NewInternalServerError(err.Error())
 	}
+	logger.Info("Init 4 CreateManyFundosRepository", "createFundos")
 
 	if err != nil {
 		logger.Error("Error trying to CreateFundosRepository", err, "createFundos")
 		return resterrors.NewInternalServerError(err.Error())
 	}
+	logger.Info("Init 5 CreateManyFundosRepository", "createFundos")
 
 	logger.Info("CreateFundosRepository executed successfully", "createFundos")
 
