a7228063992917c3c1a9d5f5a7ea059fe3bdff32
Eduardo Akira <e.akira@live.com>
2023-11-04 07:39:08 -0300

    refactor: Remove logs create Many

--- adapter/output/repository/CreateManyFundosRepository.go
+++ adapter/output/repository/CreateManyFundosRepository.go
@@ -23,23 +23,13 @@ func (ur *fundosRepository) CreateManyFundosRepository(
 
 	copier.Copy(&entity, &fundosDomain)
 	fmt.Println(fundosDomain)
-	logger.Info("Init 2 CreateManyFundosRepository", "createFundos")
 
 	_, err := collection.InsertMany(context.Background(), entity)
-	logger.Info("Init 3 CreateManyFundosRepository", "createFundos")
 
 	if err != nil {
 		logger.Error("Error CreateManyFundosRepository", err, "createFundos")
 		return resterrors.NewInternalServerError(err.Error())
 	}
-	logger.Info("Init 4 CreateManyFundosRepository", "createFundos")
-
-	if err != nil {
-		logger.Error("Error trying to CreateFundosRepository", err, "createFundos")
-		return resterrors.NewInternalServerError(err.Error())
-	}
-	logger.Info("Init 5 CreateManyFundosRepository", "createFundos")
-
 	logger.Info("CreateFundosRepository executed successfully", "createFundos")
 
 	return nil
