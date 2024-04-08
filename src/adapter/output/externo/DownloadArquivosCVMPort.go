b1de950771efd3c934ce2ecaaed75f95d4f69beb
Eduardo Akira <e.akira@live.com>
2024-04-08 18:35:08 -0300

    feat: Adicionando as permisssões

--- src/adapter/output/externo/DownloadArquivosCVMPort.go
+++ src/adapter/output/externo/DownloadArquivosCVMPort.go
@@ -8,70 +8,71 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"path"
+	"path/filepath"
+)
+
+const (
+	permissions = 0777
 )
 
 func (fc *fundosClient) DownloadArquivosCVMPort(file string) []string {
 	logger.Info("Iniciando DownloadArquivosCVMPort", "sincronizar")
-	var nomes []string
 
-	storagePath := env.GetPathArquivosCvm() + path.Dir(file)
-
-	downloadArquivo(storagePath, file)
-
-	// Descompactando o arquivo ZIP, se necessário
-	if path.Ext(file) == ".zip" {
-		nomes = unzip(storagePath + "/" + path.Base(file))
+	storagePath := filepath.Join(env.GetPathArquivosCvm(), filepath.Dir(file))
+	err := os.MkdirAll(storagePath, permissions)
+	if err != nil {
+		logger.Error("Erro ao tentar criar a pasta do arquivo:", err, "sincronizar")
+		return nil
 	}
 
+	localFilePath := filepath.Join(storagePath, filepath.Base(file))
+	err = downloadArquivo(env.GetCvmUrl()+file, localFilePath)
+	if err != nil {
+		logger.Error("Erro ao baixar o arquivo:", err, "sincronizar")
+		return nil
+	}
+
+	var nomes []string
+	if filepath.Ext(file) == ".zip" {
+		nomes = unzip(localFilePath)
+	}
+
+	logger.Info("Download de arquivo concluído com sucesso.", "sincronizar")
 	return nomes
 }
 
-func downloadArquivo(storagePath, file string) {
-	url := env.GetCvmUrl() + file
-
-	// Fazendo a requisição HTTP para baixar o arquivo
+func downloadArquivo(url, localFilePath string) error {
 	resp, err := http.Get(url)
 	if err != nil {
-		logger.Error("Erro ao tentar baixar o arquivo:", err, "sincronizar")
+		return fmt.Errorf("erro ao tentar baixar o arquivo: %v", err)
 	}
 	defer resp.Body.Close()
 
-	// Criando o diretório de armazenamento do arquivo, se não existir
-	err = os.MkdirAll(storagePath, os.ModePerm)
+	out, err := os.Create(localFilePath)
 	if err != nil {
-		logger.Error("Erro ao tentar criar a pasta do arquivo:", err, "sincronizar")
-	}
-
-	// Criando o arquivo local
-	out, err := os.Create(storagePath + "/" + path.Base(file))
-	if err != nil {
-		logger.Error("Erro ao criar o arquivo:", err, "sincronizar")
+		return fmt.Errorf("erro ao criar o arquivo: %v", err)
 	}
 	defer out.Close()
 
-	// Copiando o conteúdo do arquivo baixado para o arquivo local
 	_, err = io.Copy(out, resp.Body)
 	if err != nil {
-		logger.Error("Erro ao salvar o arquivo:", err, "sincronizar")
+		return fmt.Errorf("erro ao salvar o arquivo: %v", err)
 	}
-	logger.Info("Download de arquivo concluído com sucesso.", "sincronizar")
+
+	return nil
 }
 
-// unzip descompacta um arquivo ZIP especificado e salva seus conteúdos no sistema de arquivos local.
 func unzip(filename string) []string {
-	// Abre o arquivo ZIP para leitura
 	reader, err := zip.OpenReader(filename)
 	if err != nil {
 		logger.Error(fmt.Sprintf("Erro ao abrir arquivo ZIP: %s", filename), err, "sincronizar")
+		return nil
 	}
 	defer reader.Close()
 
 	var nomes []string
 
-	// Itera sobre todos os arquivos no arquivo ZIP
 	for _, file := range reader.File {
-		// Abre o arquivo dentro do arquivo ZIP
 		in, err := file.Open()
 		if err != nil {
 			logger.Error("Erro ao abrir arquivo dentro do ZIP:", err, "sincronizar")
@@ -79,10 +80,8 @@ func unzip(filename string) []string {
 		}
 		defer in.Close()
 
-		// Cria o arquivo correspondente no sistema de arquivos local
-		relname := path.Join(path.Dir(filename), file.Name)
-		dir := path.Dir(relname)
-		err = os.MkdirAll(dir, 0777)
+		relname := filepath.Join(filepath.Dir(filename), file.Name)
+		err = os.MkdirAll(filepath.Dir(relname), permissions)
 		if err != nil {
 			logger.Error("Erro ao criar diretório para o arquivo ZIP:", err, "sincronizar")
 			continue
@@ -95,7 +94,6 @@ func unzip(filename string) []string {
 		}
 		defer out.Close()
 
-		// Copia o conteúdo do arquivo dentro do arquivo ZIP para o arquivo local
 		_, err = io.Copy(out, in)
 		if err != nil {
 			logger.Error("Erro ao copiar conteúdo do arquivo dentro do ZIP:", err, "sincronizar")
