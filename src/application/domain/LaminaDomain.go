3ab939e195bafe1a7f2394248cede129e14e99f6
Eduardo Akira <e.akira@live.com>
2024-04-11 10:30:45 -0300

    feat: adicionando o domain de lamina

---
+++ src/application/domain/LaminaDomain.go
@@ -0,0 +1,110 @@
+package domain
+
+type LaminaDomain struct {
+	AnoAnterExemplo           string
+	AnoExemplo                string
+	AnoSemRentab              string
+	CalcRentabFundo           string
+	CalcRentabFundoGatilho    string
+	ClasseRiscoAdmin          string
+	CnpjFundo                 string
+	CondicCaren               string
+	CondicEntr                string
+	CondicSaida               string
+	ConflitoVenda             string
+	ConversaoCotaCanc         string
+	ConversaoCotaCompra       string
+	DenomSocial               string
+	DerivProtecaoCarteira     string
+	DistribGestorUnico        string
+	DsRentabGatilho           string
+	DtComptc                  string
+	DtFimDespesa              string
+	DtIniAtiv5Ano             string
+	DtIniDespesa              string
+	EnderEletronico           string
+	EnderEletronicoDespesa    string
+	EnderEletronicoReclamacao string
+	HoraAplicResgate          string
+	IndiceRefer               string
+	InvestAdic                string
+	InvestInicialMin          string
+	NmFantasia                string
+	Objetivo                  string
+	PolitInvest               string
+	PrAtivoEmissor            string
+	PrPlAlavanc               string
+	PrPlAplicMaxFundoUnico    string
+	PrPlAtivoCredPriv         string
+	PrPlAtivoExterior         string
+	PrPlDespesa               string
+	PrRentabFundo5Ano         string
+	PrVariacaoIndiceRefer5Ano string
+	PrVariacaoPerfm           string
+	PublicoAlvo               string
+	QtAnoPerda                string
+	QtDiaCaren                string
+	QtDiaConversaoCotaCompra  string
+	QtDiaConversaoCotaResgate string
+	QtDiaPagtoResgate         string
+	QtDiaSaida                string
+	RemunDistrib              string
+	RentabGatilho             string
+	ResgateMin                string
+	RestrInvest               string
+	RiscoPerda                string
+	RiscoPerdaNegativo        string
+	TaxaAdm                   string
+	TaxaAdmMax                string
+	TaxaAdmMin                string
+	TaxaAdmObs                string
+	TaxaEntr                  string
+	TaxaPerfm                 string
+	TaxaSaida                 string
+	TelSac                    string
+	TpDiaPagtoResgate         string
+	TpTaxaAdm                 string
+	VlAjustePerfmExemplo      string
+	VlDespesa3Ano             string
+	VlDespesa5Ano             string
+	VlDespesaExemplo          string
+	VlImpostoExemplo          string
+	VlMinPerman               string
+	VlPatrimLiq               string
+	VlResgateExemplo          string
+	VlRetorno3Ano             string
+	VlRetorno5Ano             string
+	VlTaxaEntrExemplo         string
+	VlTaxaSaidaExemplo        string
+	InfSac                    string
+}
+
+type LaminaCarteiraDomain struct {
+	CnpjFundo   string
+	DenomSocial string
+	DtComptc    string
+	PrPlAtivo   string
+	TpAtivo     string
+}
+
+type LaminaRentabilidadeAnoDomain struct {
+	AnoReferencia            string
+	CnpjFundo                string
+	DenomSocial              string
+	DtComptc                 string
+	PrPerfIndiceReferAno     string
+	PrRentabAno              string
+	PrVariacaoIndiceReferAno string
+	RentabAnoObservacoes     string
+}
+
+type LaminaRentabilidadeMesDomain struct {
+	CnpjFundo                string
+	DenomSocial              string
+	DtComptc                 string
+	MesReferencia            string
+	PrPerfIndiceReferMes     string
+	PrRentabMes              string
+	PrVariacaoIndiceReferMes string
+	RentabMesObservacoes     string
+}
