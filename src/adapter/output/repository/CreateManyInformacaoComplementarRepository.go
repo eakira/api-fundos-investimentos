df9b4786f0ba897fd78d127523d1fd6a7718a776
Eduardo Akira <e.akira@live.com>
2024-04-11 08:11:12 -0300

    feat: acerto no nome do arquivo

---
+++ src/adapter/output/repository/CreateManyInformacaoComplementarRepository.go
@@ -0,0 +1,112 @@
+package repository
+
+import (
+	"api-fundos-investimentos/adapter/output/model/entity"
+	"api-fundos-investimentos/application/domain"
+	"api-fundos-investimentos/configuration/env"
+	"api-fundos-investimentos/configuration/logger"
+	"api-fundos-investimentos/configuration/resterrors"
+	"context"
+
+	"github.com/jinzhu/copier"
+)
+
+func (ur *fundosRepository) CreateManyInformacaoComplementarFundoRepository(
+	domain []domain.InformacoesFundoDomain,
+) *resterrors.RestErr {
+
+	logger.Info("Init CreateManyInformacaoComplementarFundoRepository", "InformacaoComplementar")
+
+	collection := ur.databaseConnection.Collection(env.GetCollectionInformacaooComplementarFundo())
+
+	entity := []entity.InformacoesFundoEntity{}
+	copier.Copy(&entity, &domain)
+
+	dados := make([]interface{}, len(entity))
+	copier.Copy(&dados, &entity)
+
+	_, err := collection.InsertMany(context.Background(), dados)
+	if err != nil {
+		logger.Error("Error trying to CreateManyInformacaoComplementarFundoRepository", err, "InformacaoComplementar")
+		return resterrors.NewInternalServerError(err.Error())
+	}
+
+	logger.Info("CreateManyInformacaoComplementarFundoRepository executed successfully", "InformacaoComplementar")
+
+	return nil
+}
+
+func (ur *fundosRepository) CreateManyInformacaoComplementarDivulgacaoRepository(
+	domain []domain.InformacoesDivulgacaoDomain,
+) *resterrors.RestErr {
+
+	logger.Info("Init CreateManyInformacaoComplementarDivulgacaoRepository", "InformacaoComplementar")
+
+	collection := ur.databaseConnection.Collection(env.GetCollectionInformacaoComplementarDivulgacao())
+
+	entity := []entity.InformacoesDivulgacaoEntity{}
+	copier.Copy(&entity, &domain)
+
+	dados := make([]interface{}, len(entity))
+	copier.Copy(&dados, &entity)
+
+	_, err := collection.InsertMany(context.Background(), dados)
+	if err != nil {
+		logger.Error("Error trying to CreateManyInformacaoComplementarDivulgacaoRepository", err, "InformacaoComplementar")
+		return resterrors.NewInternalServerError(err.Error())
+	}
+
+	logger.Info("CreateManyInformacaoComplementarDivulgacaoRepository executed successfully", "InformacaoComplementar")
+
+	return nil
+}
+
+func (ur *fundosRepository) CreateManyInformacaoComplementarCotistaRepository(
+	domain []domain.InformacoesCotistaDomain,
+) *resterrors.RestErr {
+
+	logger.Info("Init CreateManyInformacaoComplementarCotistaRepository", "InformacaoComplementar")
+
+	collection := ur.databaseConnection.Collection(env.GetCollectionInformacaoComplementarCotista())
+
+	entity := []entity.InformacoesCotistaEntity{}
+	copier.Copy(&entity, &domain)
+
+	dados := make([]interface{}, len(entity))
+	copier.Copy(&dados, &entity)
+
+	_, err := collection.InsertMany(context.Background(), dados)
+	if err != nil {
+		logger.Error("Error trying to CreateManyInformacaoComplementarCotistaRepository", err, "InformacaoComplementar")
+		return resterrors.NewInternalServerError(err.Error())
+	}
+
+	logger.Info("CreateManyInformacaoComplementarCotistaRepository executed successfully", "InformacaoComplementar")
+
+	return nil
+}
+
+func (ur *fundosRepository) CreateManyInformacaoComplementarServicoPrestadoRepository(
+	domain []domain.ServicoPrestadoDomain,
+) *resterrors.RestErr {
+
+	logger.Info("Init CreateManyInformacaoComplementarServicoPrestadoRepository", "InformacaoComplementar")
+
+	collection := ur.databaseConnection.Collection(env.GetCollectionInformacaoComplementarServicoPrestado())
+
+	entity := []entity.ServicoPrestadoEntity{}
+	copier.Copy(&entity, &domain)
+
+	dados := make([]interface{}, len(entity))
+	copier.Copy(&dados, &entity)
+
+	_, err := collection.InsertMany(context.Background(), dados)
+	if err != nil {
+		logger.Error("Error trying to CreateManyInformacaoComplementarServicoPrestadoRepository", err, "InformacaoComplementar")
+		return resterrors.NewInternalServerError(err.Error())
+	}
+
+	logger.Info("CreateManyInformacaoComplementarServicoPrestadoRepository executed successfully", "InformacaoComplementar")
+
+	return nil
+}
